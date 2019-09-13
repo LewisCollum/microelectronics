7ebea234b0dd4072e96d632bf72c5aad6c1c8860
Lewis Collum <lewiscollum4@gmail.com>
2019-09-13 11:57:14 -0400

    Updated homework/1.

---
+++ homework/api_1.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+import math
+import pint
+
+unit = pint.UnitRegistry()
+
+def densityOfStates(materialConstant: float, kelvinOfSystem: float) -> float:
+    return materialConstant/unit.kelvin**(3/2)/unit.centimeters**3 * kelvinOfSystem**(3/2)
+
+class Boltzmann:        
+    @classmethod
+    def probability(cls, stateEnergy: float, kelvinOfSystem: float) -> float:
+        return math.exp(-stateEnergy*unit.eV/(kelvinOfSystem * unit.boltzmann_constant))
+
+class Silicon:
+    densityOfStatesMaterialConstant = 7.3e15
+    densityOfAtoms = 5e22
+
+    @classmethod
+    def intrinsicConcentrationFromKelvin(cls, kelvin: float) -> float:
+        siliconDensityOfStates = densityOfStates(
+            materialConstant=Silicon.densityOfStatesMaterialConstant,
+            kelvinOfSystem=kelvin)
+        distribution = Boltzmann.probability(
+            stateEnergy=1.12,
+            kelvinOfSystem=kelvin)**(1/2)
+        return siliconDensityOfStates * distribution
--- source/kelvin.py
+++ source/kelvin.py
@@ -1,4 +1,3 @@
-import constants
 class Kelvin:
     celsiusOffset = 273.15
     
