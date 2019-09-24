0d364c5ac51a51094c84798d5ae85323d9157293
Lewis Collum <lewiscollum4@gmail.com>
2019-09-23 22:47:12 -0400

    Prep for homework 2.

--- source/constants.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-from math import pi
-
-h = 6.626 * 10**-34 #Planck's
-h_bar = h/(2*pi) #Reduced Planck's
-k = 1.38 * 10**-23 #J/K Boltzmann
-k_B = 8.62 * 10**-5 #ev/K Boltzmann
-restingElectronMass = 9.11*10**-31
