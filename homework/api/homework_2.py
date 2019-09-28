3525c2214199d37c5ce421138c28f05d54c306e9
LewisCollum <lewiscollum4@gmail.com>
2019-09-28 00:35:21 -0400

    Began circuit analysis of 4.40

--- homework/api/homework_2.py
+++ homework/api/homework_2.py
@@ -1,9 +1,47 @@
 import pint
+import unittest
 
-a = 5
+unit = pint.UnitRegistry()
 
 def printEquation(tag, value, digits):
     print(f"\\noindent\\[{tag} = {value:.{digits}Lx}\\]")
 
 def printBoxedEquation(tag, value, digits):
     print("\\noindent\\[\\boxed{", f"{tag} = {value:.{digits}Lx}", "}\\]")
+
+
+class Node:
+    def __init__(self, voltage):
+        self.voltage = voltage
+
+    @classmethod
+    def fromBranchNode(cls, branch, node):
+        return Node(node.voltage + branch.drop)
+
+    @classmethod
+    def toBranchNode(cls, node, branch):
+        return Node(node.voltage - branch.drop)
+
+class Branch:
+    def __init__(self, drop):
+        self.drop = drop
+
+
+class TestBranchNode(unittest.TestCase):
+    def test_nodeFromBranchNode(self):
+        branch = Branch(1)
+        node =  Node(1)
+        newNode = Node.fromBranchNode(branch, node)
+
+        self.assertEqual(newNode.voltage, 2)
+
+    def test_nodeToBranchNode(self):
+        branch = Branch(1)
+        node =  Node(1)
+        newNode = Node.toBranchNode(node, branch)
+
+        self.assertEqual(newNode.voltage, 0)
+        
+
+if __name__=='__main__':
+    unittest.main()
